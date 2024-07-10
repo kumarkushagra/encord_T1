f5b0b3aa69c410773d9af986c9fb81a9103f4db6
kumarkushagra <kumarkushagra777@gmail.com>
2024-07-10 10:24:30 +0530

    Updating the html

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_homepage(request: Request):
